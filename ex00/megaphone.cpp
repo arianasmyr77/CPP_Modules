509efbd133927b83ac31a868d5a686e67c7b491b
arianasmyr77 <aforouzm@gmail.com>
2025-04-26 12:38:16 +0200

    juststartingfornow

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arforouz <aroforuz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 12:19:02 by arforouz          #+#    #+#             */
+/*   Updated: 2025/04/26 12:19:02 by arforouz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (0);
+}
