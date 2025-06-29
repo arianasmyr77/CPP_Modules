86313a1ba353f8fc4f0d4f450bd71c9efd31a197
Ariana <113695104+arianasmyr77@users.noreply.github.com>
2025-06-29 22:25:33 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -14,9 +14,25 @@
 #include <string>
 #include <locale>
 
+#include <iostream>
+#include <cctype>
+
+#include <iostream>
+#include <cctype>
+
 int main(int argc, char **argv)
 {
     if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return (0);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; argv[i][j]; j++)
+            std::cout << (char)std::toupper(argv[i][j]);//we cast it to char because toupper return an int
+        std::cout << " ";
+    }
+    std::cout << std::endl;
     return (0);
 }
